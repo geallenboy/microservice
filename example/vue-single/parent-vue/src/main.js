63e9326a80d1cdf1c33d3e3a13809aa90a5f568b
geallenboy <gejialun88@163.com>
2022-01-06 17:47:12 +0800

    feat: code

---
+++ example/vue-single/parent-vue/src/main.js
@@ -0,0 +1,32 @@
+import {createApp} from 'vue'
+import App from './App.vue'
+import router from './router'
+import {registerApplication,start} from 'single-spa'
+
+
+async function loadScript(url){
+  return new Promise((resolve,reject)=>{
+    let script = document.createElement('script');
+    script.src= url;
+    script.onload = resolve;
+    script.onerror = reject;
+    document.head.appendChild(script)
+  })
+}
+
+registerApplication('singleDemo',
+  async()=>{
+    console.log('加载模块')
+    await loadScript('http://localhost:10000/js/chunk-vendors.js')
+    await loadScript('http://localhost:10000/js/app.js')
+    return window.singleVue;
+  },
+  location=>location.pathname.startsWith('/vue')
+)
+start();
+
+const app = createApp(App);
+app.config.productionTip = false;
+app.use(router)
+app.mount('#app')
+
